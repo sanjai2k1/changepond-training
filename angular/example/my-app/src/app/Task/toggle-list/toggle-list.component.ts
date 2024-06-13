f0a07f5332bd99bb70844cc9eda6492e92cc0bd5
sanjai2k1 <sanjai2k1j@gmail.com>
2024-06-13 22:30:10 +0530

    angular tasks completed

--- angular/example/my-app/src/app/Task/toggle-list/toggle-list.component.ts
+++ angular/example/my-app/src/app/Task/toggle-list/toggle-list.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class ToggleListComponent {
   bikes:string[]=['HeroHond','BMW','Yahama','JavaBike','Bajaj']
   cars:string[]=['Suzuki Swift','Hyundai','Mahindra','kia','Renault']
+  clicke:string[]=[]
+  bikeClick=()=> this.clicke=this.bikes
+  carClick=()=> this.clicke=this.cars
+  liClick=(item : string)=>this.data=item
+  data:string=""
+
+
 }
