1c9c3bd69e0fa78834dbbe0658cbc3e79ebfe3be
sanjai2k1 <sanjai2k1j@gmail.com>
2024-06-18 10:48:22 +0530

    react

--- angular/example/my-app/src/app/shared/Guards/auth.guard.ts
+++ angular/example/my-app/src/app/shared/Guards/auth.guard.ts
@@ -1,7 +1,9 @@
 import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  
   if(typeof sessionStorage!=="undefined" && !!sessionStorage.getItem("user")){
+   
     return true
   }
   return false
