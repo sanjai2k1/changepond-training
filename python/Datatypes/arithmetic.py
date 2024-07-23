0d8616622857a8468d5520b7742002f67bbbaee8
sanjai2k1 <sanjai2k1j@gmail.com>
2024-07-23 17:33:07 +0530

    py

---
+++ python/Datatypes/arithmetic.py
@@ -0,0 +1,12 @@
+def Addition(value_1,value_2):
+    Ans = value_1+value_2
+    return Ans
+def Subraction(value_1,value_2):
+    Ans = value_1-value_2
+    return Ans
+def Multiplication(value_1,value_2):
+    Ans = value_1*value_2
+    return Ans
+def Division(value_1,value_2):
+    Ans = value_1/value_2
+    return Ans
---
+++ python/Datatypes/calculator.py
@@ -0,0 +1,27 @@
+import arithmetic
+
+def main():
+
+    print('Choose operators')
+    print('1.Add 2 Sub 3.Mul 4.Div')
+    select = int(input('select 1/2/3 : '))
+    num01= int(input('enter 1st number : '))
+    num_02 = int(input("enter 2nd number : "))
+    if (select==1):
+        Ans = arithmetic.Addition(num01,num_02)
+        print(f' {num01} + {num_02} = {Ans}')
+    elif (select==2):
+        Ans = arithmetic.Subraction(num01,num_02)
+        print(f' {num01} - {num_02} = {Ans}')
+    elif (select==3):
+        Ans = arithmetic.Multiplication(num01,num_02)
+        print(f' {num01} * {num_02} = {Ans}')
+    elif(select==4):
+        Ans = arithmetic.Division(num01,num_02)
+        print(f' {num01} / {num_02} = {Ans}')
+    else:
+        print('choose 1/2/3')
+        
+        
+if __name__ == "__main__":
+    main()
