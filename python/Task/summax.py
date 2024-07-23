b148d022d6b30dd87852d7ae0ebd48752256bb75
sanjai2k1 <sanjai2k1j@gmail.com>
2024-07-23 18:06:15 +0530

    cmt

--- python/Task/summax.py
+++ python/Task/summax.py
@@ -1,18 +1,23 @@
+def sum_of_lst(elements):
+    total=0
+    for i in elements:
+        total+=i
+    return total
+def max_of_lst(elements):
+    maxi=0
+    for i in elements:
+        if i>maxi:
+            maxi=i
+    return maxi
 def main():
     size=int(input("enter size : "))
     elements=[]
     for i in range(size):
         element = int(input(f'enter element {i+1} : '))
         elements.append(element)
-    total =0 
-    for i in elements:
-        total+=i
+    total = sum_of_lst(elements)
     print("sum is : ",total)
-    
-    maxi=0
-    for i in elements:
-        if i>maxi:
-            maxi=i
+    maxi=max_of_lst(elements)
     print("maximum element is ",maxi)
         
 
